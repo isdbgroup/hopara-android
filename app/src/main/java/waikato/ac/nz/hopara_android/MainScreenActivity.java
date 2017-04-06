10621f20689bb06593e589a8789d29824461c8de
marcus <mthn1@students.waikato.ac.nz>
2017-04-07 01:20:51 +1200

    activities with text and buttons added event handler in mainscreen activity button event listener needs to be completed

--- app/src/main/java/waikato/ac/nz/hopara_android/MainScreenActivity.java
+++ app/src/main/java/waikato/ac/nz/hopara_android/MainScreenActivity.java
@@ -2,6 +2,8 @@ package waikato.ac.nz.hopara_android;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainScreenActivity extends AppCompatActivity {
 
@@ -9,5 +11,16 @@ public class MainScreenActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_screen);
+
+        Button butTerminology = (Button) findViewById(R.id.butTerminology);
+        butTerminology.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
     }
+
+
 }
