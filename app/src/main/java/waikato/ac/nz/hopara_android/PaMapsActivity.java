2a9f5598917efe7e51bde4a7a9ac936ce079b0d8
marcus <mthn1@students.waikato.ac.nz>
2017-04-08 18:46:03 +1200

    api config cleanupo

--- app/src/main/java/waikato/ac/nz/hopara_android/PaMapsActivity.java
+++ app/src/main/java/waikato/ac/nz/hopara_android/PaMapsActivity.java
@@ -2,17 +2,30 @@ package waikato.ac.nz.hopara_android;
 
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.util.Log;
 
+import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.LatLngBounds;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
+
+import junit.framework.Assert;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 public class PaMapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
+    private Map<String, LatLng> coords;
+    private ArrayList<Marker> markers;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,8 +52,37 @@ public class PaMapsActivity extends FragmentActivity implements OnMapReadyCallba
         mMap = googleMap;
 
         // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        //        LatLng sydney = new LatLng(-34, 151);
+        //        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+        //        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+
+        //
+        setUpMap();
+
+    }
+
+    private void setUpMap(){
+
+        // add markers
+        coords = new HashMap<String, LatLng>();
+        coords.put("Pukete Pa", new LatLng(-37.737051, 175.237474));
+        coords.put("Kirikiriroa Pa", new LatLng(-37.782704, 175.28051));
+        coords.put("Te Owhanga Pa", new LatLng(-37.737971, 175.251428));
+        coords.put("Matakanoki Pa", new LatLng(-37.763188, 175.264215));
+
+        markers = new ArrayList<Marker>();
+        LatLngBounds.Builder builder = new LatLngBounds.Builder();
+        for(String key : coords.keySet()) {
+            LatLng lm = coords.get(key);
+            Marker m = mMap.addMarker(new MarkerOptions().position(lm));
+            Assert.assertNotNull(m);
+            markers.add(m);
+            builder.include(m.getPosition());
+        }
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(markers.get(0).getPosition()));
+        // zoom in on marker group
+        //LatLngBounds bounds = builder.build();
+        //mMap.moveCamera(CameraUpdateFactory.newLatLngBounds(bounds, 20));
+
     }
 }
